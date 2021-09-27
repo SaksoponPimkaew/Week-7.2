2ed4d7c12c474f460f03248e095fe305320cd579
SAKSOPON PIMKAEW <64010942@KMITL.AC.TH>
2021-09-27 17:16:48 +0700

    Add project files.

---
+++ Week 7.2/Week 7.2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    double a, b;
+    printf("Enter A & B :");
+    scanf_s("%lf %lf", &a, &b);
+    a = a* b;
+    b = a/b;
+    a = a/b;
+    printf("A= %lf\n", a);
+    printf("B= %lf\n", b);
+    return 0;
+}
+
